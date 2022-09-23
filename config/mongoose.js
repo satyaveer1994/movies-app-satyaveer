afdb7541531412f62a34ca1d73b478f89120def6
satyaveer1994 <satyaveerjat1994@gmail.com>
2022-09-23 15:40:30 +0530

    ok

---
+++ config/mongoose.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+mongoose
+  .connect(process.env.MONGODB_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connecting to MongoDB ');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+
+
+
+
+
+
